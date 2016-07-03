4c412406d103d5d40ebd834fd4726c377ebb706c
Collin Forrester <collin.forrester@gmail.com>
2016-07-03 15:44:14 -0500

    initial 50npm.sh

---
+++ 50npm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+. /opt/elasticbeanstalk/env.vars
+function error_exit
+{
+  eventHelper.py --msg "$1" --severity ERROR
+  exit $2
+}
+
+
+#avoid long NPM fetch hangups
+npm config set fetch-retry-maxtimeout 15000
+
+#install other global stuff
+#type -P phantomjs  && echo "... found, skipping install"  || {
+#npm install -g --production phantomjs@">=1.9.6 <2.0.0" --user 'root'
+#npm install -g --production casperjs --user 'root'
+#}
+
+#install not-installed yet app node_modules
+if [ ! -d "/var/node_modules" ]; then
+  mkdir /var/node_modules ;
+fi
+if [ -d /tmp/deployment/application ]; then
+  ln -s /var/node_modules /tmp/deployment/application/
+fi
+
+echo "------------------------------ — Installing/updating NPM modules, it might take a while, go take a leak or have a healthy snack... — -----------------------------------"
+OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install --production) || error_exit "Failed to run npm install.  $OUT" $?
+echo $OUT
+
+chmod -R o+r /var/node_modules
